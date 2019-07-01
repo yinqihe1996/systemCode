acb440aa95415a37f73113a9b468d7435e4e4c64
yinqihe1996 <15145905461@163.com>
2019-07-01 21:49:43 +0800

    c

---
+++ src/main/resources/Car/java.java
@@ -0,0 +1,17 @@
+public class Car {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void onClick(View v) {
+    	// TODO Auto-generated method stub
+    	Log.d(ACTIVITY_SERVICE, "Pressed the button");
+    	}
+   
+}
